e0a1e02443f6c2d9ab01649115af8ec9bfcb93da
Aaron <apurjh@gmail.com>
2016-09-11 15:16:26 +0100

    task 10.7

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,8 +3,19 @@ var cardTwo = "Queen";
 var cardThree = "King";
 var cardFour = "King";
 
-if (cardTwo === cardFour) {
+/**if (cardTwo === cardFour) {
 	alert("You found a match!");
 } else {
 	alert("Sorry, try again.")
+}**/
+
+var createBoard = function(){
+	var board = document.getElementById("game-board");
+	for (var i = 0; i < 4; i++) {
+		var cardEl = document.createElement("div");
+		cardEl.className = "card";
+		board.appendChild(cardEl);
+	}
 }
+
+createBoard();
